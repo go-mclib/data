c0d2883776efd065c1ec44b1a38beacf584fbf95
Kevin Svitač <kevin@svit.ac>
2026-02-02 11:42:13 +0100

    data gen

--- server_reports/generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-java -DbundlerMainClass="net.minecraft.data.Main" -jar ../proxy/server/server.jar --server --reports
---
+++ vanilla_server_reports/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+java -DbundlerMainClass="net.minecraft.data.Main" -jar ../proxy/server/server.jar --server --reports
+
+ln -sf "$PWD/generated/reports/blocks.json" ../pkg/data/json_reports/blocks.json
+ln -sf "$PWD/generated/reports/items.json" ../pkg/data/json_reports/items.json
+ln -sf "$PWD/generated/reports/registries.json" ../pkg/data/json_reports/registries.json
