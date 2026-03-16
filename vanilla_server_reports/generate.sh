15893b703562197b853348f2b7b4a1959c30adcd
Kevin Svitač <kevin@svit.ac>
2026-03-16 13:30:58 +0100

    generate commands too

--- vanilla_server_reports/generate.sh
+++ vanilla_server_reports/generate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-java -DbundlerMainClass="net.minecraft.data.Main" -jar ../packets_test/proxy/server/server.jar --server --reports
+java -DbundlerMainClass="net.minecraft.data.Main" -jar ../proxy/server/server.jar --server --reports
 
+ln -sf "$PWD/generated/reports/commands.json" ../pkg/data/generate/commands.json
 ln -sf "$PWD/generated/reports/blocks.json" ../pkg/data/generate/blocks.json
 ln -sf "$PWD/generated/reports/items.json" ../pkg/data/generate/items.json
 ln -sf "$PWD/generated/reports/registries.json" ../pkg/data/generate/registries.json
